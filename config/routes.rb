346b2f9f199aa291aa85f2a53db1a5a1898ff982
August Estidola <aie.estidola@gmail.com>
2023-10-19 14:00:52 +0800

    set news#index as root

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'api/news#index'
+
+  namespace :api do
+    resources :news, only: :index
+  end
 end
